b56d29c253e3f26b1288095ad0b5f8dcd73e2b65
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-23 03:11:58 -0800

    adding the set up of firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyADx3J9Jww7BucGUZlDqCq9uc9a6ukRIy8',
+      authDomain: 'shivvyauth.firebaseapp.com',
+      databaseURL: 'https://shivvyauth.firebaseio.com',
+      projectId: 'shivvyauth',
+      storageBucket: 'shivvyauth.appspot.com',
+      messagingSenderId: '616929488519'
+    });
+  }
+
   render() {
     return (
       <View>
+        <Header headerText="Authentication"/>
         <Text>An App!</Text>
       </View>
     )
