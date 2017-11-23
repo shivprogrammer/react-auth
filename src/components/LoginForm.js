3f06b2dffbc14de8606cb63d0f3058c9e7390850
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-23 03:21:03 -0800

    added LoginForm to App, and more updates to LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
+import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
-      <View />
+      <Card>
+        <CardSection>
+          <TextInput style={{ height: 20, width: 100 }} />
+        </CardSection>
+        <CardSection />
+
+        <CardSection>
+          <Button>
+            Log In
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
